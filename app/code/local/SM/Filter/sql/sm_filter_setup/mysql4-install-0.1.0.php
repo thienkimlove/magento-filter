9fdec766531fe5123ea14d38a8216fc0e2d4a448
thienkimlove <thienkimlove@gmail.com>
2015-04-17 03:17:34 +0000

    magento sm filter

---
+++ app/code/local/SM/Filter/sql/sm_filter_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MANHQUAN
+ * Date: 4/16/2015
+ * Time: 4:35 PM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
